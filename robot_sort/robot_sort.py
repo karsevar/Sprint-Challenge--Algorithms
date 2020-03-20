b4e46c9c599809a06828ff024f6bdc209aa1fb8d
masonkarsevar <masonkarsevar@gmail.com>
2020-03-20 10:50:38 -0700

    robot_sort.py only passes two tests

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -113,31 +113,44 @@ class SortingRobot:
             # while loop back to beginning of the list self.move_left()
             # self.set_light_off()
 
-        for _ in range(10):
+        for i in range(100):
+            # print(i)
+            # print(self._light)
 
             if self.light_is_on() == False:
                 while self.can_move_right() == True:
-                    print('\n', self._list, '\n')
+                    # print('\n', self._list, '\n')
+                    # print('position', self._position)
                     if self.compare_item() == None:
                         self.swap_item()
                         self.move_right()
+                    # What I'm holding is greater
                     elif self.compare_item() == 1:
                         self.swap_item()
                         self.move_left()
+                    # What I'm holding is less
+                    elif self.compare_item() == -1:
+                        self.move_left()
                         self.swap_item()
                         self.move_right()
-                    elif self.compare_item() == -1:
                         self.move_right()
                 self.set_light_on()
-            
-            else:
+            elif self.light_is_on() == True:
+                self.swap_item()
                 while self.can_move_left() == True:
                     self.move_left()
+
                 self.set_light_off()
 
+# None, 2, 3, 1
+# 2, 3, 5, 1
+# 2, 3, 1, 5
+# 2, 3, 1, None
+print(len([20, 77, 45, 16, 15, 91, 12, 6, 24, 89, 53, 19, 85, 56, 13, 74, 48, 98, 9, 92, 25, 35, 54, 44, 50, 5, 75, 34, 2, 42, 87, 49, 76, 52, 43, 23, 7, 80, 66, 14, 46, 90, 88, 40, 97, 10, 57, 63, 1, 18, 67, 79, 96, 27, 73, 28, 32, 61, 30, 8, 17, 93, 26, 51, 60, 55, 62, 31, 47, 64, 39, 22, 99, 95, 83, 70, 38, 69, 36, 41, 37, 65, 84, 3, 29, 58, 0, 94, 4, 11, 33, 86, 21, 81, 72, 82, 59, 71, 68, 78]))
 
                         
-
+# item = 5
+# position = 2
 
         
             
@@ -147,8 +160,10 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    # l = [5,3,1,2,4]
+    l = [20, 77, 45, 16, 15, 91, 12, 6, 24, 89, 53, 19, 85, 56, 13, 74, 48, 98, 9, 92, 25, 35, 54, 44, 50, 5, 75, 34, 2, 42, 87, 49, 76, 52, 43, 23, 7, 80, 66, 14, 46, 90, 88, 40, 97, 10, 57, 63, 1, 18, 67, 79, 96, 27, 73, 28, 32, 61, 30, 8, 17, 93, 26, 51, 60, 55, 62, 31, 47, 64, 39, 22, 99, 95, 83, 70, 38, 69, 36, 41, 37, 65, 84, 3, 29, 58, 0, 94, 4, 11, 33, 86, 21, 81, 72, 82, 59, 71, 68, 78]
+    print(sorted(l))
     robot = SortingRobot(l)
 
     robot.sort()
